50e3b698e2b4dba6073a80fcec558d3306ff2bcd
Kamil Bura <88596098+KamilBura@users.noreply.github.com>
2023-02-16 16:02:58 +0100

    16.02.2023 / Umschreiben
    + Handy Funktioniert
    + Automatische Auflösung funktioniert
    + Remodezined

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
     $("a").click(function(e) {
         e.preventDefault();
+    if (!$(this).hasClass("nav-no-active")) {
+      $("#framechange").attr("src", $(this).attr("href"));
+      $("a").removeClass("nav-active ");
+      $(this).addClass("nav-active ");
+        }
+    });
+});
 
-        $("#framechange").attr("src", $(this).attr("href"));
-        $("a").removeClass("nav-active ");
-        $(this).addClass("nav-active ");
-    })
-})
+function changeIframeSrc(newSrc) {
+    // zmień adres URL strony z nowym adresem URL zawierającym nazwę pliku w src
+    var newUrl = "/lernvideos-v2a/" + newSrc;
+    window.location.href = newUrl;
+  }
